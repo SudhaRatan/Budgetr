7dd68be8cd85a0c5814ce3fda15990d9f025efea
SudhaRatan <rattankumar2511@gmail.com>
2024-08-05 23:04:07 +0530

    Template (added header props types)

--- components/CustomHeader.tsx
+++ components/CustomHeader.tsx
@@ -1,8 +1,13 @@
 import { router } from 'expo-router'
 import { View } from 'react-native'
 import { Appbar, HeadlineProps, Text } from 'react-native-paper'
+import { NativeStackHeaderProps } from "@react-navigation/native-stack/src/types"
 
-const CustomHeader = ({ title, back, route }: any) => {
+interface CustomHeaderProps extends NativeStackHeaderProps {
+    title: string
+}
+
+const CustomHeader = ({ title, back }: CustomHeaderProps) => {
     return (
         <Appbar.Header>
             {back && <Appbar.BackAction onPress={() => router.back()} />}
